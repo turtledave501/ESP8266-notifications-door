b3afc90c7bc97182bd913a6049cc7aefdc76993d
turtledave501 <57016791+turtledave501@users.noreply.github.com>
2023-04-25 21:11:58 +0200

    Reading info from config.txt file

--- Garage_Door/Garage_Door.ino
+++ Garage_Door/Garage_Door.ino
@@ -1,17 +1,32 @@
 #include <ESP8266WiFi.h>
 
-const char* ssid = "";
-const char* password = "";
-const char* host = "maker.ifttt.com";
-const char* apiKey = "";
+//Looking for config.txt file, then looking for ssid, pws, api and inputing them as chars
+#include <iostream>
+#include <fstream>
+#include <string>
 
+int main() {
+    std::ifstream inputFile("config.txt");
+    std::string line;
+    std::string ssid, password, apiKey;
 
-void setup() {
-  // put your setup code here, to run once:
+    while (std::getline(inputFile, line)) {
+        if (line.find("ssid") != std::string::npos) {
+            ssid = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
+        }
+        else if (line.find("password") != std::string::npos) {
+            password = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
+        }
+        else if (line.find("apiKey") != std::string::npos) {
+            apiKey = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
+        }
+    }
 
-}
+    //Using the extracted values and defined host
+    const char* ssid = ssid.c_str();
+    const char* password = password.c_str();
+    const char* apiKey = apiKey.c_str();
+    const char* host = "maker.ifttt.com";
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
 }
