a484bd60b09ea6ceca389ce4f01cccf795ea34af
turtledave501 <57016791+turtledave501@users.noreply.github.com>
2023-04-25 20:06:45 +0200

    FirstCommit
    Start of the project

---
+++ Garage_Door/Garage_Door.ino
@@ -0,0 +1,17 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "";
+const char* password = "";
+const char* host = "maker.ifttt.com";
+const char* apiKey = "";
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
