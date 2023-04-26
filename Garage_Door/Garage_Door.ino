aac5da0c826a5c489b758a3bae5db7200194810d
turtledave501 <dcurda06@gmail.com>
2023-04-26 09:19:32 +0200

    config file reading fix

--- Garage_Door/Garage_Door.ino
+++ Garage_Door/Garage_Door.ino
@@ -22,24 +22,24 @@ const long interval = 1500;
 int main() {
     std::ifstream inputFile("privconfig.txt");
     std::string line;
-    std::string ssid, password, apiKey;
+    std::string ssid_str, password_str, apiKey_str;
 
     while (std::getline(inputFile, line)) {
         if (line.find("ssid") != std::string::npos) {
-            ssid = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
+            ssid_str = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
         }
         else if (line.find("password") != std::string::npos) {
-            password = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
+            password_str = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
         }
         else if (line.find("apiKey") != std::string::npos) {
-            apiKey = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
+            apiKey_str = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
         }
     }
 
     //Using the extracted values and defined host
-    const char* ssid = ssid.c_str();
-    const char* password = password.c_str();
-    const char* apiKey = apiKey.c_str();
+    const char* ssid = ssid_str.c_str();
+    const char* password = password_str.c_str();
+    const char* apiKey = apiKey_str.c_str();
     const char* host = "maker.ifttt.com";
 
 }
