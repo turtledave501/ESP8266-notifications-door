7ca25795f3cc55728285b542ed88c27d3c2897dc
turtledave501 <57016791+turtledave501@users.noreply.github.com>
2023-04-26 20:46:25 +0200

    Whatsapp notification setup

---
+++ UsingESP8266/Whatsapp-notify/Whatsapp-notify.ino
@@ -0,0 +1,10 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <UrlEncode.h>
+
+const char* Wifi_Name = "";
+const char* Wifi_Password = "";
+
+String phone_num = "";
+String API = "";
