d50a064850536b2414b63fbcd49c97bf7a9a7156
turtledave501 <dcurda06@gmail.com>
2023-04-26 08:39:39 +0200

    Pins and bools

--- Garage_Door/Garage_Door.ino
+++ Garage_Door/Garage_Door.ino
@@ -1,5 +1,15 @@
 #include <ESP8266WiFi.h>
 
+const int reedSwitch = 4;
+const int led = 2; //optional
+
+// Detects whenever the door changed state
+bool changeState = false;
+
+// Holds reedswitch state (1=opened, 0=close)
+bool state;
+String doorState;
+
 //Looking for config.txt file, then looking for ssid, pws, api and inputing them as chars
 #include <iostream>
 #include <fstream>
@@ -29,9 +39,9 @@ int main() {
     const char* host = "maker.ifttt.com";
 
 }
-
+//store bool in RAM
 RAM_ATTR void changeDoorStatus() {
   Serial.println("Door status changed");
   doorState = true;
-
 }
+
